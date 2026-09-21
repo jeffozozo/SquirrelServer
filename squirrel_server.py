b8e2375b5d01e027fc4b03704e1754faeffd5f2d
jeffcompas <jeffozozo@gmail.com>
2026-09-21 10:41:17 -0600

    updated network

--- squirrel_server.py
+++ squirrel_server.py
@@ -123,7 +123,7 @@ class SquirrelServerHandler(BaseHTTPRequestHandler):
         self.wfile.write(bytes("404 Not Found", "utf-8"))
 
 def run():
-    print("squirrel_server running at 127.0.0.1:8080")
+    print("squirrel_server running at 0.0.0.0:8080")
     listen = ("127.0.0.1", 8080)
     server = HTTPServer(listen, SquirrelServerHandler)
     server.serve_forever()
